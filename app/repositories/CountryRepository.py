ac09d195600de91c5da866be16794b10d6e1c6da
Davi Gomes <davirosagomesvini@gmail.com>
2025-03-21 12:07:16 -0300

    Update project with main

--- app/repositories/CountryRepository.py
+++ app/repositories/CountryRepository.py
@@ -1,20 +1,71 @@
 from typing import List, Optional
-from app.schemas.entities.Country import Country
+from sqlmodel import Session, create_engine, select
+import os
+from dotenv import load_dotenv
+
+from app.schemas.country import Country
+
 
 class CountryRepository:
     def __init__(self):
-        self.__countries: List[Country] = []
+        load_dotenv()
 
-    def save(self, country: Country) -> Country:
-        self.__countries.append(country)
-        return country
+        db_uri = os.getenv("DATABASE_URL")
+        if not db_uri:
+            raise ValueError("DATABASE_URL not found.")
 
-    def find_all(self) -> List[Country]:
-        return self.__countries
+        self.engine = create_engine(db_uri, echo=True)
 
-    def delete(self, name: str) -> Optional[Country]:
-        for country in self.__countries:
-            if country.name == name:
-                self.__countries.remove(country)
+    def _get_session(self) -> Session:
+        return Session(self.engine)
+
+    def create(self, name: str) -> Country:
+        """Create a new country in database."""
+        with self._get_session() as session:
+            country = Country(name=name)
+            session.add(country)
+            session.commit()
+            session.refresh(country)
+            return country
+
+    def get_by_id(self, country_id: int) -> Optional[Country]:
+        """Search for a country by its ID."""
+        with self._get_session() as session:
+            country = session.get(Country, country_id)
+            return country
+
+    def get_by_name(self, name: str) -> Optional[Country]:
+        """Search for a country by its name."""
+        with self._get_session() as session:
+            statement = select(Country).where(Country.name == name)
+            country = session.exec(statement).first()
+            return country
+
+    def get_all(self) -> List[Country]:
+        """Returns all countries in database."""
+        with self._get_session() as session:
+            statement = select(Country)
+            countries = session.exec(statement).all()
+            return countries
+
+    def update(self, country_id: int, name: str) -> Optional[Country]:
+        """Update the name of an existing country."""
+        with self._get_session() as session:
+            country = session.get(Country, country_id)
+            if country:
+                country.name = name
+                session.add(country)
+                session.commit()
+                session.refresh(country)
                 return country
-        return None
+            return None
+
+    def delete(self, country_id: int) -> bool:
+        """Delete a country by its ID. Returns Tue if successful, False if not found."""
+        with self._get_session() as session:
+            country = session.get(Country, country_id)
+            if country:
+                session.delete(country)
+                session.commit()
+                return True
+            return False
---
+++ app/schemas/player.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from typing import Optional
+from sqlmodel import SQLModel, Field, Relationship
+
+from app.schemas.country import Country
+from app.schemas.team import Team
+
+class Player(SQLModel, table=True):
+    """Player object."""
+    __tablename__ = "players"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(max_length=100, nullable=False)
+    birth_date: Optional[datetime] = Field(default=None)
+    country_id: int = Field(foreign_key="country.id", nullable=False)
+    position_id: int = Field(foreign_key="position.id", nullable=False)
+    team_id: Optional[int] = Field(default=None, foreign_key="team.id")
+    country: Optional["Country"] = Relationship(back_populates="players")
+    position: Optional["Position"] = Relationship(back_populates="players")
+    team: Optional["Team"] = Relationship(back_populates="players")
+
+class Position(SQLModel, table=True):
+    """Player position object."""
+    __tablename__ = "positions"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(max_length=50, unique=True, nullable=False)
