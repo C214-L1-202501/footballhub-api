6428efc4d50a9846294de1668636909438010487
Mathz <mathlimapereira@gmail.com>
2025-03-20 22:47:32 -0300

    chore: adding vscode settings for tests and debug

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from typing import Union
 
-from fastapi import FastAPI # type: ignore
+import uvicorn
+
+from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -13,3 +15,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
