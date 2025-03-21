fbce5a4a1b9a8b6d4939a6631e740fdecbeab41d
Davi Gomes <davirosagomesvini@gmail.com>
2025-03-21 12:08:27 -0300

    Update project with main

--- app/repositories/PositionRepository.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from app.schemas.entities.Position import Position
-
-class PositionRepository:
-    def __init__(self):
-        self.__positions: List[Position] = []
-
-    def save(self, position: Position) -> Position:
-        self.__positions.append(position)
