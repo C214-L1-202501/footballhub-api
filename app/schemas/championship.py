e11156f44a8723aed891b5ce541f852a8246be56
Davi Rosa Gomes <126982048+DaviRGomes@users.noreply.github.com>
2025-05-20 14:47:03 -0300

    Add route for Championship and update the Country entity (#14)
    * Add route for Championship and update the Country entity
    
    * Fix circular import between Country and Championship

--- app/schemas/championship.py
+++ app/schemas/championship.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field, Relationship
 
-from app.schemas.country import Country
+
 
 
 class Championship(SQLModel, table=True):
@@ -10,13 +10,8 @@ class Championship(SQLModel, table=True):
 
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(max_length=100, nullable=False)
-    country_id: Optional[int] = Field(default=None, foreign_key="country.id")
+    country_id: Optional[int] = Field(default=None, foreign_key="countries.id")
     type: Optional[str] = Field(max_length=50, default=None)
     season: Optional[str] = Field(max_length=20, default=None)
 
     country: Optional["Country"] = Relationship(back_populates="championships")
-
-
-Country.championships = Relationship(
-    back_populates="country", sa_relationship_kwargs={"lazy": "selectin"}
-)
